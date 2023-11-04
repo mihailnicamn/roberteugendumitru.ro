9a27051df16d701305cb790b27e66a8b8255233b
Mihail Nica <mihail.nica@gosocialdev.eu>
2023-11-04 17:21:49 +0200

    test
    test

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "apps": [
+        {
+            "name": "my-app",
+            "script": "npm",
+            "args": "start",
+            "instances": 1,
+        }
+    ]
+}
